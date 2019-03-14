98f133d9a663219f81af563766111a1478da9327
liuliang <454493675@qq.com>
2019-03-14 15:22:23 +0800

    修改数量成功

--- application/api/controller/Cart.php
+++ application/api/controller/Cart.php
@@ -4,6 +4,7 @@ namespace app\api\controller;
 use app\api\model\BaseUser;
 use app\api\model\CartDB;
 use app\api\validate\CartListValidate;
+use app\api\validate\CartModifValidate;
 use think\Controller;
 
 /**
@@ -38,7 +39,7 @@ class Cart extends Controller
         $passData = input('get.');
 
         $validate = new CartListValidate();
-        if ($validate ->check($passData) == false)
+        if ($validate ->check($passData) )
         {
             return showJson([], 400,true, $validate->getError());
         }
@@ -59,7 +60,58 @@ class Cart extends Controller
         return showJson($result);
     }
 
-    public function modifQuantity() {
 
+    /**
+     * @title  修改购物车数量
+     * @description
+     * @author 微笑城
+     * @url /api/cart/modifQuantity
+     * @method POST
+     * @param name:id type:int require:1 default:1 other: desc:唯一ID
+     * Date: 2019-03-14
+     * Time: 14:52
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * @return array:数组值
+     */
+    public function modifQuantity() {
+        if ($this->request->isPost() == false)
+        {
+            return showJson([],400, true,'请使用post 进行网络请求');
+        }
+        $passData = input('post.');
+        $validata = new CartModifValidate();
+        if ($validata->check($passData) == false)
+        {
+            return showJson([],400, true, $validata->getError());
+        }
+
+        # 根据用户id 查询 商品的id
+        $user_id = BaseUser::where(['token' => $passData['token']])->find()->getData('user_id');
+        dump($user_id);
+        if (empty($user_id))
+        {
+            return showJson([],400, true,'用户不存在');
+        }
+
+        # 根据user_id 和购物车id  查询商品
+        $result = CartDB::where(['user_id' => $user_id , 'cart_id' => $passData['cart_id']])->find();
+        if (empty($result))
+        {
+            return showJson([], 400 , true ,'购物车id不存在');
+        }
+
+        # 开始更新购物车数量
+        $cart = new CartDB();
+        $isSuccess = $cart::update(['product_id' => $passData['cart_id'] , 'quantity' => $passData['quantity']],['cart_id' => $result['cart_id']]);
+        if ($isSuccess)
+        {
+            return showJson([]);
+        }
+        else
+        {
+            return showJson([],400, true,'更新数量失败');
+        }
     }
 }
