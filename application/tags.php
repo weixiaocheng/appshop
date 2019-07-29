c6b5b4e5fd9814ac9e88e2a3c079ec271d29b300
Leah <leahshui@163.com>
2019-07-29 23:24:54 +0800

    添加跨域

--- application/tags.php
+++ application/tags.php
@@ -13,7 +13,8 @@
 return [
     // 应用初始化
     'app_init'     => [
-        'app\\api\\behavior\\CORS'
+        'app\\api\\behavior\\CORS',
+        'app\\admin\\behavior\\CORS'
     ],
     // 应用开始
     'app_begin'    => [],
