b60a13d78ce310410a19d8acef84a00da73171df
Leah <leahshui@163.com>
2019-08-23 23:26:32 +0800

    调试 拦截

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -5,7 +5,7 @@ use think\Model;
 
 class User extends Model
 {
-    protected $table = 'admin_base_user';
+    protected $table = 'admin_user';
     protected $connection = 'db_config';
     protected $pk = 'user_id';
 }
