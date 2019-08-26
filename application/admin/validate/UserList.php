1ecf7de41ef8bf73651eefcb8b6bd4b3e2b2d46d
Leah <leahshui@163.com>
2019-08-26 18:45:03 +0800

    非要添加一个新的类

---
+++ application/admin/validate/UserList.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2019/8/26
+ * Time: 下午6:44
+ */
+
+namespace app\admin\validate;
+
+use think\Validate;
+
+class UserList extends Validate
+{
+    protected  $rule = [
+        'token' => 'require',
+        'page_size' => 'number|between:1, 20',
+        'page_index' => 'require|number'
+    ];
+
+    protected $message = [
+        'token.require' => 'token 不能为空',
+        'page_size.number' => '每页长度必须为数字',
+        'page_size.between' => '每页最多有20行',
+        'page_index.number' => '起始页为1 必须为数字',
+        'page_index.require' => '起始页必须传'
+    ];
+}
