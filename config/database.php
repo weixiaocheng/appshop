914d29a132957b32dd55396ea66731c538752c13
微笑城 <454493675@qq.com>
2020-01-30 13:55:53 +0800

    适配数据库

--- config/database.php
+++ config/database.php
@@ -13,22 +13,22 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // // 服务器地址
-    // 'hostname'        => '111.230.31.176',
+    'hostname'        => '111.230.31.176',
     // // 数据库名
-    // 'database'        => 'findapp',
+    'database'        => 'appshop',
     // // 用户名
-    // 'username'        => 'root',
+    'username'        => 'root',
     // // 密码
-    // 'password'        => 'liuliang',
+    'password'        => 'Qwe123456!',
 
-      // 服务器地址
-      'hostname'        => '127.0.0.1',
-      // 数据库名
-      'database'        => 'mall',
-      // 用户名
-      'username'        => 'root',
-      // 密码
-      'password'        => '12345678',
+    //   // 服务器地址
+    //   'hostname'        => '127.0.0.1',
+    //   // 数据库名
+    //   'database'        => 'mall',
+    //   // 用户名
+    //   'username'        => 'root',
+    //   // 密码
+    //   'password'        => '12345678',
 
     // 端口
     'hostport'        => '',
